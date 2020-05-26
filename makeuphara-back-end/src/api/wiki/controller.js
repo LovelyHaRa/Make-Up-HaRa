2a024c9e4f01c71f565cae81fadd2df6207df085
LovelyHaRa <codetender94@gmail.com>
2020-05-26 23:32:19 +0900

    bug fix: issues #3

--- makeuphara-back-end/src/api/wiki/controller.js
+++ makeuphara-back-end/src/api/wiki/controller.js
@@ -121,6 +121,10 @@ export const requestDocument = async (ctx) => {
 
 export const addTitle = async (ctx) => {
   const { name } = ctx.request.body;
+  if (!name) {
+    ctx.status = 400;
+    return;
+  }
   const title = new WikiTitle({
     name,
     lately: 0,
@@ -180,6 +184,7 @@ export const searchDocument = async (ctx, next) => {
   try {
     const documentList = await WikiTitle.find({
       name: { $regex: '.*' + query + '.*' },
+      lately: { $gt: 0 },
     })
       .sort(sortObj)
       .lean();
