340f96020bfdfdc9fa4955126f8e98584a568b4a
LovelyHaRa <codetender94@gmail.com>
2020-09-20 00:18:43 +0900

    Update Package

--- makeuphara-front-end/src/containers/profile/SecurityContainer.js
+++ makeuphara-front-end/src/containers/profile/SecurityContainer.js
@@ -96,37 +96,38 @@ const SecurityContainer = () => {
         form.newPassword !== form.curPassword,
       confirmPassword: form.confirmPassword !== '' && isValidConfirmPassword,
     });
-    // 검증 실패 시 처리
+    // 검증 메시지 처리
+    // 새로운 비밀번호 메시지 설정
     if (!isValidNewPassword && form.newPassword !== '') {
+      // 8자 이하일 때
       setValidMessage((prevState) => ({
         ...prevState,
         newPassword: '비밀번호는 8자 이상 입력해야 합니다!',
       }));
     } else if (
+      isValidNewPassword &&
       form.newPassword !== '' &&
-      form.newPassword === form.curPassword &&
-      isValidNewPassword
+      form.newPassword === form.curPassword
     ) {
+      // 기존 비밀번호와 일치할 때
       setValidMessage((prevState) => ({
         ...prevState,
         newPassword: '기존 비밀번호와 일치합니다.',
       }));
-    } else if (form.newPassword === '' || isValidNewPassword) {
-      setValidMessage((prevState) => ({
-        ...prevState,
-        newPassword: '',
-      }));
+    } else if (isValidNewPassword || form.newPassword === '') {
+      // 검증 성공 또는 값이 없을 때 메시지 초기화
+      setValidMessage((prevState) => ({ ...prevState, newPassword: null }));
     }
+    // 새로운 비밀번호 확인 메시지 설정
     if (!isValidConfirmPassword && form.confirmPassword !== '') {
+      // 비밀번호가 일치하지 않을 때
       setValidMessage((prevState) => ({
         ...prevState,
         confirmPassword: '비밀번호가 일치하지 않습니다!',
       }));
-    } else if (form.confirmPassword === '' || isValidConfirmPassword) {
-      setValidMessage((prevState) => ({
-        ...prevState,
-        confirmPassword: '',
-      }));
+    } else if (isValidConfirmPassword || form.confirmPassword === '') {
+      // 검증 성공 또는 값이 없을 때 메시지 초기화
+      setValidMessage((prevState) => ({ ...prevState, confirmPassword: null }));
     }
   }, [form]);
 
@@ -143,17 +144,11 @@ const SecurityContainer = () => {
         changePasswordError.response &&
         changePasswordError.response.status === 412
       ) {
-        setSubmitPassword({
-          result: false,
-          message: null,
-        });
+        setSubmitPassword({ result: false, message: null });
+        setIsValid((prevState) => ({ ...prevState, curPassword: false }));
         setValidMessage((prevState) => ({
           ...prevState,
-          curPassword: '기존 비밀번호가 일치하지 않습니다',
-        }));
-        setIsValid((prevState) => ({
-          ...prevState,
-          curPassword: false,
+          curPassword: '기존 비밀번호가 일치하지 않습니다.',
         }));
       } else {
         setSubmitPassword({
