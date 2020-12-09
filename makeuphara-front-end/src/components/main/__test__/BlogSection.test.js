b2c12cf5087b62afe3932a08a231a7b77c8e14ec
LovelyHaRa <codetender94@gmail.com>
2020-12-09 12:18:27 +0900

    feat: add BlogSection testing code

---
+++ makeuphara-front-end/src/components/main/__test__/BlogSection.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { render } from '@testing-library/react';
+
+import BlogSection from '../BlogSection';
+import { samplePostList as postList } from '../../../lib/data/test';
+
+const props = { loading: false, error: null };
+
+describe('<BlogSection />', () => {
+  const renderWithRouter = ({ postList, props }) =>
+    render(<BlogSection postList={postList} {...props} />, {
+      wrapper: MemoryRouter,
+    });
+
+  it('should be render if post list is exist', () => {
+    const { getByText } = renderWithRouter({ postList, props });
+
+    expect(getByText('최근 등록된 포스트')).toBeInTheDocument();
+    postList.map(({ title }) => expect(getByText(title)).toBeInTheDocument());
+    expect(getByText('MORE POST...')).toBeInTheDocument();
+  });
+
+  it('should be render when data is loading', () => {
+    const { getByText, queryByText } = renderWithRouter({
+      postList,
+      props: { ...props, loading: true },
+    });
+
+    expect(getByText('최근 등록된 포스트')).toBeInTheDocument();
+    postList.map(({ title }) =>
+      expect(queryByText(title)).not.toBeInTheDocument(),
+    );
+    expect(getByText('MORE POST...')).toBeInTheDocument();
+  });
+
+  it('should be render if data fetching is failed', () => {
+    const { getByText } = renderWithRouter({
+      postList,
+      props: { ...props, error: { message: 'Not Found' } },
+    });
+
+    expect(getByText('블로그 리스트 요청 실패.')).toBeInTheDocument();
+    expect(getByText('ERROR MESSAGE: Not Found')).toBeInTheDocument();
+  });
+});
---
+++ makeuphara-front-end/src/lib/data/test.js
@@ -0,0 +1,77 @@
+export const samplePostList = [
+  {
+    _id: '5f4aabf91266254d6cb1d1c7',
+    tags: ['test'],
+    title: 'test12',
+    body: 'test1212',
+    publisher: {
+      _id: '5ed8faf8d3fb0639901ba49d',
+      username: 'master',
+      name: 'master',
+    },
+    publishedDate: '2020-08-29T19:26:49.382Z',
+    comment: [],
+    __v: 0,
+  },
+  {
+    _id: '5ed8fbdcd3fb0639901ba4a8',
+    tags: ['test11'],
+    title: 'test11',
+    body: 'test11',
+    publisher: {
+      _id: '5ed8faf8d3fb0639901ba49d',
+      username: 'master',
+      name: 'master',
+    },
+    publishedDate: '2020-06-04T13:49:16.401Z',
+    comment: [],
+    __v: 0,
+  },
+  {
+    _id: '5ed8fbc8d3fb0639901ba4a7',
+    tags: [],
+    title: 'test10',
+    body:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500...",
+    publisher: {
+      _id: '5ed8faf8d3fb0639901ba49d',
+      username: 'master',
+      name: 'master',
+    },
+    publishedDate: '2020-06-04T13:48:56.739Z',
+    comment: [],
+    __v: 0,
+  },
+  {
+    _id: '5ed8fbc2d3fb0639901ba4a6',
+    tags: [],
+    title: 'test9',
+    body:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500...",
+    publisher: {
+      _id: '5ed8faf8d3fb0639901ba49d',
+      username: 'master',
+      name: 'master',
+    },
+    publishedDate: '2020-06-04T13:48:50.317Z',
+    comment: [],
+    __v: 0,
+  },
+  {
+    _id: '5ed8fbb6d3fb0639901ba4a5',
+    tags: ['test8'],
+    title: 'test8',
+    body:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500...",
+    publisher: {
+      _id: '5ed8faf8d3fb0639901ba49d',
+      username: 'master',
+      name: 'master',
+    },
+    publishedDate: '2020-06-04T13:48:38.412Z',
+    comment: [],
+    __v: 0,
+  },
+];
+
+export const sampleDocumentList = [];
